d70d2ee278bb3a8954c078e3d8888a6a3a11edfa
ivan <ivan08021991@gmail.com>
2021-11-11 16:55:29 +0300

    новый метод

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,33 @@
 import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-       Scanner scan = new Scanner(System.in);
-       System.out.print("Enter the String: ");
-       String str = scan.nextLine();
-       int len = str.length();
-       System.out.println("Lenght of String = " +len);
+        Scanner sc = new Scanner(System.in);
+       boolean result = true;
+        while (result){
+        System.out.print("Enter the String:");
+        String s = sc.nextLine();
+        if (s.equals("exit")) {
+            result = false;
+        }
+        int len = s.length();
+        System.out.println("lenght of String = " + len);
+        }
 
     }
-}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
